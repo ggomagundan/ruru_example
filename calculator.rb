7548267b2c99262ac10d898bcda2523cf682b60b
Zack Siri <zack@codemy.net>
2016-07-12 00:34:43 +0700

    update example

---
+++ calculator.rb
@@ -0,0 +1,31 @@
+require 'benchmark'
+require 'fiddle'
+
+
+library = Fiddle::dlopen('target/release/librusty_calculator.dylib')
+
+Fiddle::Function.new(library['initialize_my_app'], [], Fiddle::TYPE_VOIDP).call
+
+
+class Calculator
+  def pow_3(number)
+    (1..number).each_with_object({}) do |index, hash|
+      hash[index] = index ** 3
+    end
+  end
+end
+
+# ... somewhere in the application code ...
+Benchmark.bm do |x|
+  x.report {
+    5_000_000.times do 
+      Calculator.new.pow_3(5)
+    end
+  }
+
+  x.report { 
+    5_000_000.times do 
+      RustyCalculator.new.pow_3(5)
+    end
+  }
+end
