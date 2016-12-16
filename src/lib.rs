5ef028d2e301a53b7c018b1f26e76f5553eb8201
ggomagundan <pbs52@hanmail.net>
2016-12-16 14:36:36 +0900

    modify code compact for ruru version

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,21 @@
-#![no_std]
-
 #[macro_use]
 extern crate ruru;
 
+use std::error::Error;
+/*
 use ruru::VM;
 use ruru::Hash;
 use ruru::Fixnum;
 use ruru::Class;
 use ruru::AnyObject;
-use ruru::types::{Argc, Value};
-use ruru::traits::Object;
+*/
+// Merge above ruru Structs
+use ruru::{VM, Hash, Fixnum, Class, Object};
+
+// ruru Changes and deprecated
+// https://github.com/d-unseductable/ruru/wiki/Upgrading-from-0.7-to-0.8
+// use ruru::types::{Argc, Value};
+// use ruru::traits::Object; .
 
 class!(RustyCalculator);
 
@@ -20,7 +26,13 @@ methods!(
     fn pow_3(num: Fixnum) -> Hash {
         let mut hash = Hash::new();
 
-        for i in 1..num.to_i64() + 1 {
+        // `num` is not a valid `Fixnum`, raise an exception
+        if let Err(ref error) = num {
+            VM::raise(error.to_exception(), error.description());
+        }
+
+        // We can safely unwrap here, because an exception was raised if `num` is `Err`
+        for i in 1..num.unwrap().to_i64() + 1 {
             hash.store(Fixnum::new(i), Fixnum::new(i.pow(3)));
         }
 
@@ -30,7 +42,7 @@ methods!(
 
 #[no_mangle]
 pub extern fn initialize_my_app() {
-    Class::new("RustyCalculator").define(|itself| {
+    Class::new("RustyCalculator", None).define(|itself| {
         itself.def("pow_3", pow_3);
     });
 }
